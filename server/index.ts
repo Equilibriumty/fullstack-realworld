c1615410a42fbb1a1118e4158dcfe1ee487354b2
Equilibriumty <fanflackplay@gmail.com>
2024-10-22 23:31:46 +0300

    feat: add docker-compose, resolve deps

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,6 @@ const app = new Hono();
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
-
 const port = Number(process.env["PORT"]) || 3000;
 
 console.log(`Server is running on port ${port}`);
